680336d7d0299b01c90c906ba6ef71eede199aff
= <luis019dell@gmail.com>
2024-11-27 13:19:11 -0600

    Ejercico 5 empezado

---
+++ src/Ejercicio_5/ConversorDeFormatos.java
@@ -0,0 +1,14 @@
+package Ejercicio_5;
+
+public class ConversorDeFormatos {
+    private String ruta;
+
+    public ConversorDeFormatos(String ruta){
+        this.ruta = ruta;
+    }
+
+    public
+
+
+
+}
