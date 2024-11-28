9e13129034183181878a6213ef2ce6359b398c6e
luis50019 <teddiazdiaz019@gmail.com>
2024-11-28 06:38:44 -0600

    Ejercicio 5 terminado

--- src/Ejercicio_5/ConversorDeFormatos.java
+++ src/Ejercicio_5/ConversorDeFormatos.java
@@ -1,7 +1,9 @@
 package Ejercicio_5;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,6 +20,7 @@ public class ConversorDeFormatos {
 
     public ConversorDeFormatos(String ruta){
         this.ruta = ruta;
+        this.data = new ArrayList<>();
     }
 
     public void leerArchivosCSV(){
@@ -25,7 +28,8 @@ public class ConversorDeFormatos {
         try(BufferedReader archivo = new BufferedReader((new FileReader(this.ruta)))){
             linea = archivo.readLine();
             String[] columnas = linea.split(",");
-            encabezados = new String[columnas.length];
+            this.encabezados = new String[columnas.length];
+            this.encabezados = columnas;
 
             while((linea = archivo.readLine())!= null){
                 Map<String,String> persona = new HashMap<>();
@@ -33,6 +37,7 @@ public class ConversorDeFormatos {
                 for(int i=0; i<encabezados.length;i++){
                     persona.put(encabezados[i],columnas[i]);
                 }
+                data.add(persona);
             }
             return ;
         }catch (Exception e){
@@ -46,13 +51,45 @@ public class ConversorDeFormatos {
 //            + "\"ciudad\": \"Madrid\""
 //            + "}";
 //
-    private String JSONparse(List<Map<String,String>> data){
-        String objeto = "{ ";
+    private String JSONparse(){
 
+        String objeto = "[ ";
+        int numeroObjetos = this.data.size();
+        int indiceObjetos = 0;
+        for(Map<String,String>objetoPersona: this.data){
+            String subObjeto = "{";
+            int longitudDelObjeto = objetoPersona.size();
+            int indice = 0;
+            for(String llave: objetoPersona.keySet() ){
+                subObjeto += "\""+llave+"\""+":"+"\""+objetoPersona.get(llave)+"\"";
 
-
+                if(indice < longitudDelObjeto -1){
+                    subObjeto += ",";
+                }
+                indice ++;
+            }
+            objeto += "\n"+"    "+subObjeto+"}";
+            if(indiceObjetos < numeroObjetos -1){
+                objeto += ",";
+            }
+            indiceObjetos++;
+        }
+        objeto += "\n]";
         return objeto;
     }
+    private void escribirJSON(String data){
+        try(BufferedWriter escritor = new BufferedWriter(new FileWriter("datos.json"))){
+            escritor.write(data);
+            System.out.println("\nSe acabo\n");
+        }catch (Exception e){
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
 
+    public void convertirArchivosCSV(){
+        leerArchivosCSV();
+        String data = JSONparse();
+        escribirJSON(data);
+    }
 
 }
